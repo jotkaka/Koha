546601ec936c83f8e721d89ca76345f94bd8128b
Janusz Kaczmarek <januszop@gmail.com>
2025-01-15 18:50:50 +0000

    Poprawienie kodowania eksportu - NFC zamiast NFD

--- opac/opac-export.pl
+++ opac/opac-export.pl
@@ -114,7 +114,7 @@ if ( $format =~ /endnote/ ) {
     $marc   = marc2endnote($marc);
     $format = 'endnote';
 } elsif ( $format =~ /marcxml/ ) {
-    $marc   = marc2marcxml($marc);
+    $marc   = marc2marcxml( $marc, undef, undef, 1 );
     $format = 'marcxml';
 } elsif ( $format =~ /mods/ ) {
     $marc   = marc2modsxml($marc);
@@ -135,11 +135,11 @@ if ( $format =~ /endnote/ ) {
     $marc   = $marc->as_usmarc() unless $error;
     $format = 'marc8';
 } elsif ( $format =~ /utf8/ ) {
-    C4::Charset::SetUTF8Flag( $marc, 1 );
+    C4::Charset::SetUTF8Flag( $marc, 0 );
     $marc   = $marc->as_usmarc();
     $format = 'utf8';
 } elsif ( $format =~ /marcstd/ ) {
-    C4::Charset::SetUTF8Flag( $marc, 1 );
+    C4::Charset::SetUTF8Flag( $marc, 0 );
     ( $error, $marc ) = marc2marc( $marc, 'marcstd', C4::Context->preference('marcflavour') );
     $format = 'marcstd';
 } elsif ( $format =~ /isbd/ ) {
