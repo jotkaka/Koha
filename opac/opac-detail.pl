0f1de49d891311ae6ff3ea2df52d8a92928e323d
Janusz Kaczmarek <januszop@gmail.com>
2025-01-21 21:52:06 +0100

    Pelniejsze wyswietlanie informacji z OPACAuthorIdentifiersAndInformation

--- opac/opac-detail.pl
+++ opac/opac-detail.pl
@@ -1297,9 +1297,11 @@ if ( C4::Context->preference('OPACAuthorIdentifiersAndInformation') ) {
         next unless $authority;
         my $information = $authority->get_identifiers_and_information;
         next unless $information;
-        my ($name) =
-            map { $_->{value} }
-            grep { $_->{code} eq 'a' ? $_ : () } @{ $author->{MARCAUTHOR_SUBFIELDS_LOOP} };
+        my ($name) = join(
+            ' ',
+            map      { $_->{value} }
+                grep { $_->{code} =~ /[abcdn]/ ? $_ : () } @{ $author->{MARCAUTHOR_SUBFIELDS_LOOP} }
+        );
         push @author_information,
             { authid => $authid, name => $name, information => $information };
     }
