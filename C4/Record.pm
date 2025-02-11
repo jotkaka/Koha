9633d7546f5290ccdbbfe1bf099e2490803871ee
Janusz Kaczmarek <januszop@gmail.com>
2025-02-11 10:56:24 +0100

    Pozostawianie pol 920, 924 i 930 przy czyszczeniu rekordu przez C4::Record::marc2marc

--- C4/Record.pm
+++ C4/Record.pm
@@ -99,6 +99,7 @@ sub marc2marc {
                 if ( $field->tag() =~ m/9/
                     && ( $field->tag() != '490' || C4::Context->preference("marcflavour") eq 'UNIMARC' ) )
                 {
+                    next if grep { $field->tag eq $_ } qw/ 920 924 930 /;
                     push @privatefields, $field;
                 } elsif ( !( $field->is_control_field() ) ) {
                     $field->delete_subfield( code => '9' ) if ( $field->subfield('9') );
